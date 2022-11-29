d68e21cd3814d019d9eb136770921ee7dcbd0d5d
dlddu <39251873+dlddu@users.noreply.github.com>
2022-11-29 22:25:56 +0900

    StartProgramUseCase 작성

---
+++ MemoStack/UseCase/StartProgram.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MemoStack.UseCase;
+
+public interface IRepository
+{
+    IEnumerable<MemoModel> GetMemos();
+}
+
+public class MemoModel
+{
+    public MemoModel(string text, int depth)
+    {
+        Text = text;
+        Depth = depth;
+    }
+
+    public string Text { get; set; }
+    internal int Depth { get; set; }
+}
+
+public class StartProgramUseCase
+{
+    private readonly IRepository _repository;
+
+    public StartProgramUseCase(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public Stack<MemoModel> Invoke()
+    {
+        var memos = _repository.GetMemos().ToList();
+        var stack = new Stack<MemoModel>(memos.Count);
+        foreach (var memo in memos.OrderBy(memo => memo.Depth)) stack.Push(memo);
+
+        return stack;
+    }
+}
