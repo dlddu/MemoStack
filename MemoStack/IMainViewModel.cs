21b6ee47758f4f46fedec2a643b09b47e95845dd
dlddu <39251873+dlddu@users.noreply.github.com>
2022-11-29 22:32:38 +0900

    IMainViewModel 작성 후 View에 연결

---
+++ MemoStack/IMainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Input;
+using MemoStack.UseCase;
+
+namespace MemoStack;
+
+public interface IMainViewModel
+{
+    public MemoModel PoppedMemoModel { get; }
+    public ICommand CreateMemoCommand { get; }
+    public ICommand DeleteMemoCommand { get; }
+}
