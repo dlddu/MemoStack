8b36faacde1808d9c01cf6a14c534cfa47c1beb3
dlddu <39251873+dlddu@users.noreply.github.com>
2022-12-01 21:14:09 +0900

    MainViewModel 작성 및 연결

--- MemoStack/Repository/MemoContext.cs
+++ MemoStack/Repository/MemoContext.cs
@@ -1,14 +1,21 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using MemoStack.UseCase;
 using Microsoft.EntityFrameworkCore;
 
 namespace MemoStack.Repository;
 
-public class MemoContext : DbContext
+public class MemoContext : DbContext, IRepository
 {
     private readonly string _dbPath = Path.Join(".", "memo.db");
 
     public DbSet<MemoModel> MemoModels { get; set; }
+
+    public IEnumerable<MemoModel> GetMemos()
+    {
+        return MemoModels.ToList();
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
---
+++ MemoStack/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Windows.Input;
+using MemoStack.Repository;
+using MemoStack.UseCase;
+
+namespace MemoStack.ViewModel;
+
+public class MainViewModel : IMainViewModel
+{
+    private readonly Stack<MemoModel> _stack;
+
+    public MainViewModel()
+    {
+        using var repository = new MemoContext();
+        _stack = new StartProgramUseCase(repository).Invoke();
+        PoppedMemoModel = _stack.TryPop(out var memoModel) ? memoModel : new MemoModel(string.Empty, 0);
+    }
+
+    public MemoModel PoppedMemoModel { get; }
+    public ICommand CreateMemoCommand { get; }
+    public ICommand DeleteMemoCommand { get; }
+}
