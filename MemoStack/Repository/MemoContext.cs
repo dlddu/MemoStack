38813b786918f847d22f1e9bd58e0a8ea2bc79a8
dlddu <39251873+dlddu@users.noreply.github.com>
2022-12-01 21:00:22 +0900

    패키지 EntityFrameworkCore7, EntityFrameworkCore.Sqlite7 추가 후 MemoContext 작성

---
+++ MemoStack/Repository/MemoContext.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using MemoStack.UseCase;
+using Microsoft.EntityFrameworkCore;
+
+namespace MemoStack.Repository;
+
+public class MemoContext : DbContext
+{
+    private readonly string _dbPath = Path.Join(".", "memo.db");
+
+    public DbSet<MemoModel> MemoModels { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite($"Data Source={_dbPath}");
+    }
+}
